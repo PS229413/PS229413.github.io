d0105c62fd4cbb1fbd9f495f4f83080b7b3fe9fe
PS229413 <PS229413@users.noreply.github.com>
2024-09-11 14:21:13 +0000

    deploy: d4dee0f92e382eb47b794390d489758be60e078c

---
+++ server/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,287 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 912;
+exports.ids = [912];
+exports.modules = {
+
+/***/ 1185:
+/***/ ((module) => {
+
+module.exports = require("mongoose");
+
+/***/ }),
+
+/***/ 9491:
+/***/ ((module) => {
+
+module.exports = require("assert");
+
+/***/ }),
+
+/***/ 4300:
+/***/ ((module) => {
+
+module.exports = require("buffer");
+
+/***/ }),
+
+/***/ 6113:
+/***/ ((module) => {
+
+module.exports = require("crypto");
+
+/***/ }),
+
+/***/ 2361:
+/***/ ((module) => {
+
+module.exports = require("events");
+
+/***/ }),
+
+/***/ 3685:
+/***/ ((module) => {
+
+module.exports = require("http");
+
+/***/ }),
+
+/***/ 5687:
+/***/ ((module) => {
+
+module.exports = require("https");
+
+/***/ }),
+
+/***/ 2037:
+/***/ ((module) => {
+
+module.exports = require("os");
+
+/***/ }),
+
+/***/ 3477:
+/***/ ((module) => {
+
+module.exports = require("querystring");
+
+/***/ }),
+
+/***/ 7310:
+/***/ ((module) => {
+
+module.exports = require("url");
+
+/***/ }),
+
+/***/ 3837:
+/***/ ((module) => {
+
+module.exports = require("util");
+
+/***/ }),
+
+/***/ 9796:
+/***/ ((module) => {
+
+module.exports = require("zlib");
+
+/***/ }),
+
+/***/ 5138:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  headerHooks: () => (/* binding */ headerHooks),
+  originalPathname: () => (/* binding */ originalPathname),
+  requestAsyncStorage: () => (/* binding */ requestAsyncStorage),
+  routeModule: () => (/* binding */ routeModule),
+  serverHooks: () => (/* binding */ serverHooks),
+  staticGenerationAsyncStorage: () => (/* binding */ staticGenerationAsyncStorage),
+  staticGenerationBailout: () => (/* binding */ staticGenerationBailout)
+});
+
+// NAMESPACE OBJECT: ./app/api/auth/[...nextauth]/route.js
+var route_namespaceObject = {};
+__webpack_require__.r(route_namespaceObject);
+__webpack_require__.d(route_namespaceObject, {
+  GET: () => (handler),
+  POST: () => (handler)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/node-polyfill-headers.js
+var node_polyfill_headers = __webpack_require__(2394);
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-modules/app-route/module.js
+var app_route_module = __webpack_require__(9692);
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-kind.js
+var route_kind = __webpack_require__(9513);
+// EXTERNAL MODULE: external "mongoose"
+var external_mongoose_ = __webpack_require__(1185);
+;// CONCATENATED MODULE: ./models/user.js
+
+const userSchema = new external_mongoose_.Schema({
+    email: {
+        type: String,
+        unique: [
+            true,
+            "Email already exists"
+        ],
+        required: [
+            true,
+            "Email required"
+        ]
+    },
+    username: {
+        type: String,
+        required: [
+            true,
+            "username required"
+        ],
+        match: [
+            /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+            "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"
+        ]
+    },
+    image: {
+        type: String
+    }
+});
+const User = external_mongoose_.models.User || (0,external_mongoose_.model)("User", userSchema);
+/* harmony default export */ const user = (User);
+
+// EXTERNAL MODULE: ./utils/database.js
+var database = __webpack_require__(8400);
+// EXTERNAL MODULE: ./node_modules/next-auth/next/index.js
+var next = __webpack_require__(7185);
+// EXTERNAL MODULE: ./node_modules/next-auth/providers/google.js
+var google = __webpack_require__(1989);
+;// CONCATENATED MODULE: ./app/api/auth/[...nextauth]/route.js
+
+
+
+
+const handler = (0,next/* default */.ZP)({
+    // Configure one or more authentication providers
+    providers: [
+        (0,google/* default */.Z)({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks: {
+        async signIn ({ profile }) {
+            console.log("JUST SIGNED IN USING GOOGLE");
+            try {
+                await (0,database/* connectToDB */.P)();
+                // Check if the user already exist
+                const userExists = await user.findOne({
+                    email: profile.email
+                });
+                // If not create a user
+                if (!userExists) {
+                    await user.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    });
+                }
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+        },
+        async session ({ session }) {
+            console.log("JUST STARTED/CREATED A SESSION");
+            const sessionUser = await user.findOne({
+                email: session.user.email
+            });
+            session.user.id = sessionUser._id.toString();
+            return session;
+        }
+    }
+});
+
+
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-app-loader.js?page=%2Fapi%2Fauth%2F%5B...nextauth%5D%2Froute&name=app%2Fapi%2Fauth%2F%5B...nextauth%5D%2Froute&pagePath=private-next-app-dir%2Fapi%2Fauth%2F%5B...nextauth%5D%2Froute.js&appDir=%2Fhome%2Frunner%2Fwork%2FPS229413.github.io%2FPS229413.github.io%2Fapp&appPaths=%2Fapi%2Fauth%2F%5B...nextauth%5D%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D!
+
+// @ts-ignore this need to be imported from next/dist to be external
+
+
+// @ts-expect-error - replaced by webpack/turbopack loader
+
+const AppRouteRouteModule = app_route_module.AppRouteRouteModule;
+// We inject the nextConfigOutput here so that we can use them in the route
+// module.
+const nextConfigOutput = ""
+const routeModule = new AppRouteRouteModule({
+    definition: {
+        kind: route_kind.RouteKind.APP_ROUTE,
+        page: "/api/auth/[...nextauth]/route",
+        pathname: "/api/auth/[...nextauth]",
+        filename: "route",
+        bundlePath: "app/api/auth/[...nextauth]/route"
+    },
+    resolvedPagePath: "/home/runner/work/PS229413.github.io/PS229413.github.io/app/api/auth/[...nextauth]/route.js",
+    nextConfigOutput,
+    userland: route_namespaceObject
+});
+// Pull out the exports that we need to expose from the module. This should
+// be eliminated when we've moved the other routes to the new format. These
+// are used to hook into the route.
+const { requestAsyncStorage , staticGenerationAsyncStorage , serverHooks , headerHooks , staticGenerationBailout  } = routeModule;
+const originalPathname = "/api/auth/[...nextauth]/route";
+
+
+//# sourceMappingURL=app-route.js.map
+
+/***/ }),
+
+/***/ 8400:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   P: () => (/* binding */ connectToDB)
+/* harmony export */ });
+/* harmony import */ var mongoose__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1185);
+/* harmony import */ var mongoose__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(mongoose__WEBPACK_IMPORTED_MODULE_0__);
+
+let isConnected = false;
+const connectToDB = async ()=>{
+    mongoose__WEBPACK_IMPORTED_MODULE_0___default().set("strictQuery", true);
+    if (isConnected) {
+        console.log("MongoDB is already connected");
+        return;
+    }
+    try {
+        await mongoose__WEBPACK_IMPORTED_MODULE_0___default().connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        isConnected = true;
+        console.log("MONGODB CONNECTED");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [478,501,733], () => (__webpack_exec__(5138)));
+module.exports = __webpack_exports__;
+
+})();
