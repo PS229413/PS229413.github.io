676f094aa47ab90ca458a17248225558222328b6
PS229413 <PS229413@summacollege.nl>
2024-09-12 12:58:31 +0200

    latest commit

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
-    }
+    },
 }
-
 module.exports = {
     output: 'export', // Allows Next.js to export static HTML
     distDir: 'build', // Optional: to specify a custom build directory
   };
+
